774a970c326f6b1f9f8050893df90c113e24cfd1
foo <odk.mmmm@gmail.com>
2016-08-13 14:00:25 +0000

    パスワードリセット機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'password_resets/new'
 
-  get 'users/new'
+  get 'password_resets/edit'
 
-  root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
   #get 'static_pages/help'
-  get 'signup'  => 'users#new'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
