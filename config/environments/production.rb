420e62fd8df7c48b5f588df518a88b5f9e957f6a
foo <odk.mmmm@gmail.com>
2016-08-13 15:10:44 +0000

    herokuの設定のミスを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app55078630@heroku.com'],
+    :password       => ENV['juc5rl2g4179'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
