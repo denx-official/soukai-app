900b68f402201b6356ec33d443e7fbb2b9f2172b
foo <odk.regis@gmail.com>
2017-04-01 14:38:37 +0000

    新歓用に

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # ページごとの完全なタイトルを返す
   def full_title(page_title = '')
-    base_title = "DENX 総会app"
+    base_title = "DENX APP"
     
     if page_title.empty?
       base_title
